d87b8358d5f7c5f7ce0e08c5117c5b8374a8e9ab
odfloress <odfloress@unah.hn>
2022-05-31 23:22:07 -0600

    31 de mayo se edito

--- resources/views/admin/catalogo/index_oficina.blade.php
+++ resources/views/admin/catalogo/index_oficina.blade.php
@@ -12,7 +12,7 @@
     <div class="container mt-3">
         <h3>Administrar Oficina</h3> <br>  
         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
-            Editar
+            Edita
         </button>
     </div>
 
